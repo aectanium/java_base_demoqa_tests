bc680002cca786c033a971b415aca52edf8347d3
Dyakonov <aectan1987@ya.ru>
2026-02-13 14:13:36 +0300

    Added TestBase

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,23 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "144.0.7559.133";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.timeout = 10000;
+    }
+    @AfterEach
+    void afterEach() {
+        closeWebDriver();
+    }
+}
--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,24 +1,13 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTests {
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize ="1920x1080";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion ="144.0.7559.133";
-        Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 10000;
-    }
-    @Test
+public class TextBoxTests extends TestBase{
+@Test
     void succesfulFillFormTest() {
         open("/text-box");
         $("[id=userName]").setValue("Alex D");
